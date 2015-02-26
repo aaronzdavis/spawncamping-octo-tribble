f0305c98dfbbce5d52cc32b583b5421950539f3e
aaronzdavis <aaronzdavis@gmail.com>
2015-02-26 11:35:30 -0800

    add tests

---
+++ spec/models/sandwich_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Sandwich, type: :model do
+
+  it 'should be valid' do
+    expect(Sandwich.new()).to be_valid
+  end
+
+  it 'should respond to name' do
+    expect(Sandwich.new()).to respond_to(:name)
+  end
+
+end
